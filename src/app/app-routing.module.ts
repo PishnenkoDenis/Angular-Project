cef22d1480b3e3a0bf50886f85c4f5b4ab4f21a6
d.pishnenko <d.pishnenko@tg.dunice.net>
2022-09-30 15:29:16 +0300

    Add routing between components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'sign_in', component: SignInComponent},
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
