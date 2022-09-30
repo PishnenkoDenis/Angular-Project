44baeb38dc2142c65e0723ad7e0e87f8a3d804ef
d.pishnenko <d.pishnenko@tg.dunice.net>
2022-09-30 11:02:21 +0300

    Add components into app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
